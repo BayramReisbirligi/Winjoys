de63684226773ea2f0b2571ca54951cbb7a6c617
Bayram Reis <bayramreisbirligi@gmail.com>
2025-07-29 19:39:50 +0300

    Initial commit

---
+++ Enums/ActionType.cs
@@ -0,0 +1,9 @@
+﻿namespace ReisProduction.Winjoys.Enums;
+public enum ActionType : ushort
+{
+    SendGameKey = 0x01,
+    SendInput = 0x02,
+    KybdEvent = 0x03,
+    PostMessage = 0x04,
+    SendMessage = 0x05,
+}
